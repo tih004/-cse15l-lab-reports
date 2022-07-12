2fc882f67ace4454709f83539c0cbc00e4e8f42f
tih004 <108309275+tih004@users.noreply.github.com>
2022-07-12 09:10:26 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,28 +23,28 @@ public class MarkdownParseTest {
     // @Test
     // public void testTestFile2() throws IOException{
 	// 	List<String> StringInput = List.of("https://google.com", "some-thing.html");
-    //     List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("/Users/tiannahuang/Documents/GitHub/markdown-parser-1/test-file2.md")));
+    //     List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("test-file2.md")));
     //     assertEquals(StringInput, Actual);
     // }
 
     @Test
     public void testTestFile3() throws IOException{
 		List<String> StringInput = List.of();
-        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("/Users/tiannahuang/Documents/GitHub/markdown-parser-1/test-file3.md")));
+        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md")));
         assertEquals(StringInput, Actual);
     }
 
     @Test
     public void testTestFile5() throws IOException{
 		List<String> StringInput = List.of("page.com");
-        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("/Users/tiannahuang/Documents/GitHub/markdown-parser-1/test-file5.md")));
+        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("test-file5.md")));
         assertEquals(StringInput, Actual);
     }
 
     @Test
     public void testTestFile6() throws IOException{
 		List<String> StringInput = List.of("page.com");
-        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("/Users/tiannahuang/Documents/GitHub/markdown-parser-1/test-file6.md")));
+        List<String> Actual = MarkdownParse.getLinks(Files.readString(Path.of("test-file6.md")));
         assertEquals(StringInput, Actual);
     }
 
